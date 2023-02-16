442aa4310c2805779f2dacb3c8c99d4c0720f702
Teabenic <te3373be-s@student.lu.se>
2023-02-16 11:38:17 +0000

    Add project files.

---
+++ WebSeriveAssignment3/WebService1.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebSeriveAssignment3
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://ics.lu.se/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+      //  [WebMethod]
+      //  public string HelloWorld()
+     //   {
+      //      return "Hello World";
+
+        }
+    }
+}
