e5a495817cb0ae9353351b39ba63a4a876f5d53c
Teabenic <te3373be-s@student.lu.se>
2023-02-19 19:29:28 +0000

    metoder

---
+++ WebSeriveAssignment3/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSeriveAssignment3.Models;
+
+namespace WebSeriveAssignment3.Models
+{
+    public class Order
+    {   
+        public int OrderID { get; set; }
+        public string OrderDate { get; set; }
+        public int SupermarketID  { get; set; }
+        public int CustomerID { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
