502ff795afbfb5ba9fcc42ba173a0ef6f074e663
Nanna Amankwah <na6404am-s@student.lu.se>
2023-02-23 19:38:58 +0100

    "Lagt till Errorhandling Client 2 Färdig "

--- WinFormsClient2/Form1.cs
+++ WinFormsClient2/Form1.cs
@@ -1,6 +1,10 @@
 using ServiceReferenceForms;
+using System.Linq.Expressions;
+using System.Numerics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace WinFormsClient2
@@ -14,6 +18,7 @@ namespace WinFormsClient2
 
         private void buttonViewInformationSelect_Click(object sender, EventArgs e)
         {
+            try { 
             string viewCertainInformation = comboBoxViewInformation.SelectedItem.ToString();
 
             var endpointConfiguration = WebService1SoapClient.EndpointConfiguration.WebService1Soap;
@@ -94,78 +99,120 @@ namespace WinFormsClient2
 
                 default:
                     break;
+                }
+            }
+            catch (System.ServiceModel.FaultException ex)
+            {
+                richTextBoxViewAllInformation.Clear();
 
+                richTextBoxViewAllInformation.AppendText("Sorry, we encountered an error while processing your request. Please try again later");
+            }
+            catch (NullReferenceException ex)
+            {
+                richTextBoxViewAllInformation.Clear();
+
+                richTextBoxViewAllInformation.AppendText("Please select a web service method to call");
 
             }
-
         }
 
 
 
         private void buttonSelectFind_Click(object sender, EventArgs e)
         {
+            try {
+                string findCertainInformation = comboBoxFindByID.SelectedItem.ToString();
 
-            string findCertainInformation = comboBoxFindByID.SelectedItem.ToString();
+                var endpointConfiguration = WebService1SoapClient.EndpointConfiguration.WebService1Soap;
+                WebService1SoapClient service = new WebService1SoapClient(endpointConfiguration);
 
-            var endpointConfiguration = WebService1SoapClient.EndpointConfiguration.WebService1Soap;
-            WebService1SoapClient service = new WebService1SoapClient(endpointConfiguration);
+                string selectedOption = comboBoxFindByID.SelectedItem.ToString();
+              
 
-            string selectedOption = comboBoxFindByID.SelectedItem.ToString();
-            int input;
+                int idInput = int.Parse(textBoxFindByID.Text);
 
-            if (!int.TryParse(textBoxFindByID.Text, out input))
+                switch (selectedOption)
+                {
+                    case "Get Orders From Customer ID":
+                        List<Order> orders = service.GetOrdersFromCustomer(idInput);
+                        if (orders.Count == 0)
+                        {
+                            richTextBoxFindByID.Clear();
+                            richTextBoxFindByID.AppendText($"No Orders found for Customer ID: {idInput}.{Environment.NewLine}");
+                        }
+                        else
+                        {
+                            richTextBoxFindByID.Clear();
+                            richTextBoxFindByID.AppendText($"ORDERS FOR CUSTOMER ID {idInput}:{Environment.NewLine}");
+                            foreach (Order order in orders)
+                            {
+                                string orderInfo = $"Order ID: {order.OrderID} - Date: {order.OrderDate} - Supermarket ID: {order.SupermarketID} - Payment Method: {order.PaymentMethod}";
+                                richTextBoxFindByID.AppendText(orderInfo + Environment.NewLine);
+                            }
+                        }
+                        break;
+
+                    case "Get Orderlines From Order ID":
+                        List<Orderline> orderlines = service.GetOrdersLinesFromOrder(idInput);
+                        if (orderlines.Count == 0)
+                        {
+                            richTextBoxFindByID.Clear();
+                            richTextBoxFindByID.AppendText($"No Orderlines found for Order ID: {idInput}.{Environment.NewLine}");
+                        }
+                        else
+                        {
+                            richTextBoxFindByID.Clear();
+                            richTextBoxFindByID.AppendText($"ORDERLINES FOR ORDER ID: {idInput}:{Environment.NewLine}");
+                            foreach (Orderline orderline in orderlines)
+                            {
+                                string orderlineInfo = $"Order ID: {orderline.OrderID} - Product ID: {orderline.ProductID} - Orderline number: {orderline.OrderlineNumber} - Quantity: {orderline.Quantity}";
+                                richTextBoxFindByID.AppendText(orderlineInfo + Environment.NewLine);
+                            }
+                        }
+                        break;
+
+                    case "Get Products From Category ID":
+                        List<Product> products = service.GetProductsFromCategory(idInput);
+                        if (products.Count == 0)
+                        {
+                            richTextBoxFindByID.Clear();
+                            richTextBoxFindByID.AppendText($"No Products found for Category ID: {idInput}.{Environment.NewLine}");
+                        }
+                        else
+                        {
+                            richTextBoxFindByID.Clear();
+                            richTextBoxFindByID.AppendText($"PRODUCTS FOR CATEGORY ID: {idInput}:{Environment.NewLine}");
+                            foreach (Product product in products)
+                            {
+                                string productInfo = $"Product ID: {product.ProductID} - Product Name: {product.ProductName} - Price: {product.Price} - Category ID: {product.CategoryID}";
+                                richTextBoxFindByID.AppendText(productInfo + Environment.NewLine);
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            } catch (FormatException ex)
             {
+                richTextBoxFindByID.Clear();
+
                 richTextBoxFindByID.AppendText("Please enter a valid Order ID, Customer ID or Category ID.");
-                return;
-            }
 
-            switch (selectedOption)
+            }
+            catch(NullReferenceException ex)
             {
-                case "Get Orders From Customer ID":
-                    List<Order> orders = service.GetOrdersFromCustomer(input);
-                    richTextBoxFindByID.Clear();
-                    richTextBoxFindByID.AppendText($"ORDERS FOR CUSTOMER ID {input}:{Environment.NewLine}");
-                    foreach (Order order in orders)
-                    {
-                        string orderInfo = $"Order ID: {order.OrderID} - Date: {order.OrderDate} - Supermarket ID: {order.SupermarketID} - Payment Method: {order.PaymentMethod}";
-                        richTextBoxFindByID.AppendText(orderInfo + Environment.NewLine);
-                    }
-                    break;
+                richTextBoxFindByID.Clear();
 
-                case "Get Orderlines From Order ID":
-                    List<Orderline> orderlines = service.GetOrdersLinesFromOrder(input);
-                    richTextBoxFindByID.Clear();
-                    richTextBoxFindByID.AppendText($"ORDERLINES FOR ORDER ID: {input}:{Environment.NewLine}");
-                    foreach (Orderline orderline in orderlines)
-                    {
-                        string orderlineInfo = $"Order ID: {orderline.OrderID} - Product ID: {orderline.ProductID} - Orderline number: {orderline.OrderlineNumber} - Quantity: {orderline.Quantity}";
-                        richTextBoxFindByID.AppendText(orderlineInfo + Environment.NewLine);
-                    }
-                    break;
-
-                case "Get Products From Category ID":
-                    List<Product> products = service.GetProductsFromCategory(input);
-                    richTextBoxFindByID.Clear();
-                    richTextBoxFindByID.AppendText($"PRODUCTS FOR CATEGORY ID: {input}:{Environment.NewLine}");
-                    foreach (Product product in products)
-                    {
-                        string productInfo = $"Product ID: {product.ProductID} - Product Name: {product.ProductName} - Price: {product.Price} - Category ID: {product.CategoryID}";
-                        richTextBoxFindByID.AppendText(productInfo + Environment.NewLine);
-                    }
-                    break;
-
-                default:
-                    MessageBox.Show("Please select an option.");
-                    break;
+                richTextBoxFindByID.AppendText("Please select a web service method to call");
+            }
+            catch(System.ServiceModel.FaultException ex)
+            {
+                richTextBoxFindByID.AppendText("Sorry, we encountered an error while processing your request. Please try again later");
             }
 
 
 
 
-
-
-
- 
         }
-    }
+        }
 }
